bbcb2cfde5a8a7b8cb9f3b52e891bd1a08539677
Kartik Bagri <kartikbagri199@gmail.com>
2021-10-09 01:43:13 +0530

    commit on 9th october

--- routes/overviewRoute.js
+++ routes/overviewRoute.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const axios = require('axios');
 
 // ********** Using Modules **********
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const payload = {title: 'Overview', userLoggedIn: req.user};
+router.get('/', async function(req, res) {
+    const payload = {
+        title: 'Overview'
+    };
+    const result = await getQuote();
+    if(result) {
+        payload.quoteText = result.q;
+        payload.quoteAuthor = result.a;
+    } else {
+        payload.quoteError = 'Unable to fetch Thought of the day';
+    }
     res.render('overview', payload);
 });
+
+async function getQuote() {
+    const res = await axios.get('https://zenquotes.io/api/random')
+    .catch(err => {
+        console.log(err);
+        return null;
+    });
+    const currQuote = res.data[0];
+    return currQuote;
+}
 
 module.exports = router;
